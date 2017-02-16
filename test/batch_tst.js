6a927732f825f6cfbee96d7d9c4f0a411ab8c616
Anton Akentiev <anthony.akentiev@gmail.com>
2017-02-16 14:59:40 +0300

    Refactoring: move tests to separate files

---
+++ test/batch_tst.js
@@ -0,0 +1,100 @@
+var server = require('../server.js');
+var db = require('../db.js');
+var db_helpers = require('../helpers/db_helpers.js');
+var config = require('../config.js');
+var helpers = require('../helpers/helpers.js');
+
+var fs = require('fs');
+var http = require('http');
+var assert = require('assert');
+
+eval(fs.readFileSync('test/helpers.js')+'');
+
+var signature = '';
+var userId = '';
+var globalToken = '';
+
+var orgOneInn = 0;
+var ticketOneId = 0;
+var ticketOneSerialNumber = 0;
+
+var INN = '1234567890';
+var TEST_SER_NUM = 'АБ123456';
+
+var batchOneId = 0;
+
+describe('Batch module',function(){
+     before(function(done){
+          var uri  = 'mongodb://localhost/tests';
+
+          var conn = db.connectToDb(uri,'','');
+          db.removeDb(function(){
+               server.initDb(db);
+
+               server.startHttp(9091);
+               done();   // ok
+          });
+     });
+
+     after(function(done){
+          server.stop();
+          db.removeDb(function(){});
+          db.disconnectDb();
+          done();
+     });
+
+     it('should create batch', function(done){
+          var url = '/api/v1/organizers/' + INN + '/batches';
+
+          var data = { 
+               number_of_tickets: 1 
+          };
+          var postData = JSON.stringify(data);
+
+          var authToken = '';
+          postDataAuth(9091,url,postData,authToken,function(err,statusCode,headers,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,200);
+
+               var p = JSON.parse(dataOut);
+               assert.notEqual(p.batch_id, 0);
+               
+               batchOneId = p.batch_id;
+
+               done();
+          });
+     })
+
+     it('should get batch', function(done){
+          var url = '/api/v1/organizers/' + INN + '/batches/' + batchOneId;
+
+          var authToken = '';
+          getData(9091,url,authToken,function(err,statusCode,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,200);
+
+               var p = JSON.parse(dataOut);
+               assert.equal(p.length,1);
+
+               ticketOneId = p[0];
+
+               done();
+          });
+     })
+
+     it('should get ticket', function(done){
+          var url = '/api/v1/organizers/' + INN + '/tickets/' + ticketOneId;
+
+          var authToken = '';
+          getData(9091,url,authToken,function(err,statusCode,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,200);
+
+               var p = JSON.parse(dataOut);
+               assert.equal(p.state,"created");
+
+               done();
+          });
+     })
+});
+
---
+++ test/serial_number_tst.js
@@ -0,0 +1,98 @@
+var server = require('../server.js');
+var db = require('../db.js');
+var db_helpers = require('../helpers/db_helpers.js');
+var config = require('../config.js');
+var helpers = require('../helpers/helpers.js');
+
+var fs = require('fs');
+var http = require('http');
+var assert = require('assert');
+
+eval(fs.readFileSync('test/helpers.js')+'');
+
+var signature = '';
+var userId = '';
+var globalToken = '';
+
+var orgOneInn = 0;
+var ticketOneId = 0;
+var ticketOneSerialNumber = 0;
+
+var INN = '1234567890';
+var TEST_SER_NUM = 'АБ123456';
+
+var batchOneId = 0;
+
+describe('Serial number propagation',function(){
+     before(function(done){
+          var uri  = 'mongodb://localhost/tests';
+
+          var conn = db.connectToDb(uri,'','');
+          db.removeDb(function(){
+               server.initDb(db);
+
+               server.startHttp(9091);
+               done();   // ok
+          });
+     });
+
+     after(function(done){
+          server.stop();
+          db.removeDb(function(){});
+          db.disconnectDb();
+          done();
+     });
+
+     it('should get no tickets', function(done){
+          var url = '/api/v1/organizers/' + INN + '/tickets';
+
+          var authToken = '';
+          getData(9091,url,authToken,function(err,statusCode,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,404);
+               done();
+          });
+     })
+
+     it('should create ticket', function(done){
+          var url = '/api/v1/organizers/' + INN + '/tickets';
+
+          var data = { 
+               serial_number: TEST_SER_NUM
+          };
+          var postData = JSON.stringify(data);
+
+          var authToken = '';
+          postDataAuth(9091,url,postData,authToken,function(err,statusCode,headers,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,200);
+
+               //console.log('Data: ');
+               //console.log(dataOut);
+
+               var p = JSON.parse(dataOut);
+               assert.notEqual(p.id,0);
+               assert.equal(p.serial_number,TEST_SER_NUM);
+
+               done();
+          });
+     })
+
+     it('should not add ticket with same SER_NUMBER', function(done){
+          var url = '/api/v1/organizers/' + INN + '/tickets';
+
+          var data = { 
+               serial_number: TEST_SER_NUM
+          };
+          var postData = JSON.stringify(data);
+
+          var authToken = '';
+          postDataAuth(9091,url,postData,authToken,function(err,statusCode,headers,dataOut){
+               assert.equal(err,null);
+               assert.equal(statusCode,409);
+
+               done();
+          });
+     })
+})
+
