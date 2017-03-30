4b85312fb8876e87424d9d61aff3b257542259c2
AnthonyAkentiev <tony@chain.cloud>
2017-03-30 09:24:06 +0000

    Updating parity scripts

--- parity/start_parity.sh
+++ parity/start_parity.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-parity --geth --jsonrpc --chain=kovan-config.json --jsonrpc-apis "web3,eth,net,personal,traces,rpc" --jsonrpc-cors "*" --jsonrpc-port "8545" --jsonrpc-hosts "all" --jsonrpc-interface "0.0.0.0" --password "parity.pass" --tx-gas-limit "5000000"
+# Default Path: 
+# ~/.local/share/io.parity.ethereum/
 
-# --unlock 0xf123
+parity --geth --jsonrpc --chain=kovan-config.json --jsonrpc-apis "web3,eth,net,personal,traces,rpc" --jsonrpc-cors "*" --jsonrpc-port "8545" --jsonrpc-hosts "all" --jsonrpc-interface "0.0.0.0" --password "parity.pass" --tx-gas-limit "5000000" --unlock 0xdf8845a26e1db43236e591a863289ad5c1fcda5a
