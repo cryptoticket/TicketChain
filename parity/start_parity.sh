198d36d32ab69e15dece21e61e4d3a6f3d1df853
AnthonyAkentiev <tony@chain.cloud>
2017-03-30 13:53:10 +0000

    Update parity script

--- parity/start_parity.sh
+++ parity/start_parity.sh
@@ -2,5 +2,10 @@
 
 # Default Path: 
 # ~/.local/share/io.parity.ethereum/
-parity --geth --jsonrpc --chain=kovan --jsonrpc-apis "web3,eth,net,personal,traces,rpc" --jsonrpc-cors "*" --jsonrpc-port "8545" --jsonrpc-hosts "all" --jsonrpc-interface "0.0.0.0" --password "parity.pass" --tx-gas-limit "5000000" --unlock 0xdf8845a26e1db43236e591a863289ad5c1fcda5a
+
+# Kovan
+parity --geth --jsonrpc --chain=kovan --jsonrpc-apis "web3,eth,net,personal,traces,rpc" --jsonrpc-cors "*" --jsonrpc-port "8545" --jsonrpc-hosts "all" --jsonrpc-interface "0.0.0.0" --password "parity.pass" --tx-gas-limit 9000000 --unlock 0xdf8845a26e1db43236e591a863289ad5c1fcda5a --force-ui --ui-interface "0.0.0.0" --ui-no-validation
+
+# Ropsten
+#parity --geth --jsonrpc --chain=ropsten --jsonrpc-apis "web3,eth,net,personal,traces,rpc" --jsonrpc-cors "*" --jsonrpc-port "8545" --jsonrpc-hosts "all" --jsonrpc-interface "0.0.0.0" --password "parity.pass" --tx-gas-limit 9000000 --force-ui --ui-interface "0.0.0.0" --ui-no-validation
 
